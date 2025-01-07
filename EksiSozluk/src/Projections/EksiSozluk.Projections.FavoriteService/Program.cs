ad52fef1f3a5dc5081a2a821be90a2c335768c45
aliyildiz <aliyyldiz@gmail.com>
2025-01-07 15:48:21 +0300

    init

---
+++ EksiSozluk/src/Projections/EksiSozluk.Projections.FavoriteService/Program.cs
@@ -0,0 +1,7 @@
+using EksiSozluk.Projections.FavoriteService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
